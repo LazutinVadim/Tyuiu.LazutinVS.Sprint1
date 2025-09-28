f70036e69bad8ee9973d58c99b15a91202de4390
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-09-28 16:45:29 +0500

    Task5

---
+++ Tyuiu.LazutinVS.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.LazutinVS.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.LazutinVS.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void VaidExpression()
+        {
+            DataService ds = new DataService();
+            int res = ds.Calculate(8);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.LazutinVS.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.LazutinVS.Sprint1.Task5.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Лазутин В. С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #17                                                             *");
+        Console.WriteLine("* Выполнил: Лазутин В. С.    | РППб-25-1                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая считает по формуле                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        int x;
+
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x));
+
+        Console.ReadLine();
+    }
+}
