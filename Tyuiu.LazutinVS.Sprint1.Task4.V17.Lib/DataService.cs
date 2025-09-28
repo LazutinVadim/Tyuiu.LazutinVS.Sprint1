96478de8c892a09a6a966d14e132e1a905500af7
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-09-28 15:55:58 +0500

    Task4

--- Tyuiu.LazutinVS.Sprint1.Task4.V17.Lib/DataService.cs
+++ Tyuiu.LazutinVS.Sprint1.Task4.V17.Lib/DataService.cs
@@ -6,16 +6,7 @@ namespace Tyuiu.LazutinVS.Sprint1.Task4.V17.Lib
     {
         public double Calculate(double x, double y)
         {
-            double den = x - 5 * y;
-            if (den < 0 || den == 0)
-            {
-                return 0;
-            }
-            else
-            { 
-                return Math.Round(1/Math.Sqrt(den),3);
-            }
-
+            return Math.Round(1 / Math.Sqrt(x - 5 * y), 3);
         }
     }
 }
